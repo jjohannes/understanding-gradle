a82571a7c601cded1519b37e0e94307a45302a65
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-06-17 13:21:08 +0200

    Convert all samples to Groovy DSL

--- 09_Centralizing_Dependency_Versions/B_Platform_Project/my-build-logic/java-plugins/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    `kotlin-dsl`
-}
-
-dependencies {
-    implementation(platform("org.example.my-app:shared-platform"))
-
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
-}
--- 28_Dependency_Analysis_Plugin/my-build-logic/java-plugins/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    `kotlin-dsl`
-}
-
-dependencies {
-    implementation("com.autonomousapps:dependency-analysis-gradle-plugin:1.25.0")
-}
